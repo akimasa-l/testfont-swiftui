f28dbe7dac774bb2a8344bd18e9c0cf2dddea872
akimasa-l <liumingzheng8@outlook.com>
2024-10-19 00:28:12 +0900

    できたじゃーーん！！

--- testfont/ContentView.swift
+++ testfont/ContentView.swift
@@ -14,6 +14,22 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            // https://softmoco.com/basics/how-to-add-custom-fonts.php
+            // ↑ここをみました
+            /*
+             ① つけたいフォントファイルをドラッグ&ドロップでこのプロジェクトのフォルダーの下に入れる
+             // https://zenn.dev/ruwatana/articles/2045140478b1de
+             ② ↑ここを参考にしてInfo.plistを生成する
+             ②-① 左のフォルダーの一番の親をダブルクリック xcodeprojファイルを開く
+             ②-② 上がGeneralになっているところをInfoにしてDocument Typesを展開し+ボタンを押す
+             ③ Info.plistのDocumentTypesはダミーなので削除して
+             ④ プラスボタンでFonts provided by applicationを選び
+             ⑤ 展開してitem0のvalueをSTKaiti.ttfにする
+             ⑥ Textに.font(.custom("STKaiti", size: 100))と書く。
+             ⑦ 終わり！お疲れ様でした！
+             */
+            Text("刘明正")
+                .font(.custom("STKaiti", size: 100))
         }
         .padding()
     }
