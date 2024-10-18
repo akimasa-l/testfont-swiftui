6c4b9289437bb1c09454b878d03f09c3f5dc0b2f
akimasa-l <liumingzheng8@outlook.com>
2024-10-18 23:04:34 +0900

    Initial Commit

---
+++ testfont/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testfont
+//
+//  Created by 劉明正 on 2024/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testfont/testfontApp.swift
@@ -0,0 +1,17 @@
+//
+//  testfontApp.swift
+//  testfont
+//
+//  Created by 劉明正 on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct testfontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
